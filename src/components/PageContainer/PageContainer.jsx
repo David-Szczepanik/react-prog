0683d3c73fd0c87be98042fc52477e08953b3246
David Szczepanik <106731343+David-Szczepanik@users.noreply.github.com>
2024-03-01 14:48:16 +0100

    Updates

--- src/components/PageContainer/PageContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./PageContainer.css";
-
-function PageContainer({ children }) {
-  return <div className="page-container">{children}</div>;
-}
-
-export default PageContainer;
